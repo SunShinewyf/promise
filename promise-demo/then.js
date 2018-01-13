973de915f745a4befef78dee696ed7049a3a49cd
SunShinewyf <2632807692@qq.com>
2018-01-13 12:08:32 +0800

    update the demo of then

--- promise-demo/then.js
+++ promise-demo/then.js
@@ -18,3 +18,17 @@ promise().then((res) => {
 }, (err) => {
     console.log('reject', err);
 })
+
+//demo2 pending状态没有任何改变，就不会进入到then的任何一个回调中
+
+let promise = function () {
+    return new Promise((resolve, reject) => {
+        console.log('I am pending');
+    })
+}
+
+promise().then((res) => {
+    console.log('resolve', res);
+}, (err) => {
+    console.log('reject', err);
+})
