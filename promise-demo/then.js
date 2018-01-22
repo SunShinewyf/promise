6e5727a1fea390ac45cc1d80ab68280316e2d1da
SunShinewyf <2632807692@qq.com>
2018-01-22 18:59:30 +0800

    add the series-promise.js

--- promise-demo/then.js
+++ promise-demo/then.js
@@ -35,13 +35,30 @@
 
 
 //demo3 then中的回调中不返回数据时会获取不到值
-let promise = new Promise((resolve, reject) => {
-    resolve('promise')
-})
+// let promise = new Promise((resolve, reject) => {
+//     resolve('promise')
+// })
 
 
-promise.then(function () {
-    //dosomething
-}).then((res) => {
-    console.log(res)
+// promise.then(function () {
+//     //dosomething
+// }).then((res) => {
+//     console.log(res)
+// })
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
+  }, 1000)
 })
+const promise2 = promise1.then(() => {
+  throw new Error('error!!!')
+})
+
+console.log('promise1', promise1)
+console.log('promise2', promise2)
+
+setTimeout(() => {
+  console.log('promise1', promise1)
+  console.log('promise2', promise2)
+}, 2000)
